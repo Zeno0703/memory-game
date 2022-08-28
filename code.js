f87ffba1c7076848d8b83128f3d3d8fcedc0dae6
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2022-08-28 22:02:05 +0200

    nice

--- code.js
+++ code.js
@@ -1,32 +1,28 @@
-let one = document.querySelector(".card-opposite:nth-child(3)");
-let two = document.querySelector(".card-opposite:nth-child(11)");
-
-console.log(one == two);
-
-
-
 const cards = document.querySelectorAll(".card");
+const turnedCards = document.querySelectorAll(".card-opposite");
+let possibilities;
 let counter = 0;
+let found = 0;
+let lost = 0;
 
-cards.forEach(card => {
-    card.onclick = turnCard(card);
-})
+
+window.onload = function(){
+    setTimeout(function(){ turnCards(turnedCards)}, 2000)
+}
+
 
 function turnCard(card){
     card.className = "card-opposite";
     counter++
     if(counter == 2){
         if (checkContent()){
-            let turned = document.querySelectorAll(".card-opposite");
-            turned.forEach(card => {
-                card.className = "card-opposite-right";
-            })
+            const turned = document.querySelectorAll(".card-opposite");
+            setTimeout(function(){ removeCards(turned)}, 1500);
         }
+
         else{
-            let turned = document.querySelectorAll(".card-opposite");
-            turned.forEach(card => {
-                card.className = "card";
-            })
+            const turned = document.querySelectorAll(".card-opposite");
+            setTimeout(function(){ turnCards(turned)}, 1500);
         }
         counter = 0;
     }
@@ -35,6 +31,50 @@ function turnCard(card){
 function checkContent(){
     let turned = document.querySelectorAll(".card-opposite");
     if (turned[0].firstChild.data == turned[1].firstChild.data){
+        found++
         return true;
     }
+    lost++
+}
+
+function removeCards(cards){
+    cards.forEach(card => {
+        card.className = "card-opposite-right";
+    })
+    if (checkWin()){
+        alert("You won!");
+    }
+}
+
+function showCards(cards){
+    cards.forEach(card => {
+        card.className = "card-opposite";
+    })
+}
+
+function turnCards(cards){
+    cards.forEach(card => {
+        card.className = "card";
+    })
+}
+
+function checkWin(){
+    const rightCards = document.querySelectorAll(".card-opposite-right");
+    return rightCards.length == 12
+}
+
+function reset(){
+    possibilities = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F"];
+    const rightCards = document.querySelectorAll(".card-opposite-right");
+    rightCards.forEach(card => {
+        card.className = "card";
+    })
+    // werkt nog niet helemaal GOTTA FIGURE THIS OUT
+    const cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        const data = possibilities[Math.floor(Math.random() * possibilities.length)];
+        card.firstChild.data = possibilities[Math.floor(Math.random() * possibilities.length)];
+        possibilities.splice(possibilities.indexOf(data), 1);
+        console.log(possibilities);
+    })
 }
